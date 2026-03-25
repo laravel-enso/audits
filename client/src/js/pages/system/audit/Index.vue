40ad17e9eea53a5896a4103447dc71cdbb87c322
Teodor Pop <tedipop16@gmail.com>
2026-03-25 15:50:56 +0200

    forgot to copy this here

--- client/src/js/pages/system/audit/Index.vue
+++ client/src/js/pages/system/audit/Index.vue
@@ -1,15 +1,28 @@
 <template>
     <enso-table class="box is-paddingless raises-on-hover"
-        id="audits"/>
+        id="audits">
+        <template #changes="{ row }">
+            <diff :event="row.event" :changes="row.changes"/>
+        </template>
+
+        <template #createdBy="{ row }">
+                <avatar class="is-24x24"
+                    tooltip
+                    :user="row.createdBy"
+                    v-if="row.createdBy"/>
+        </template>
+    </enso-table>
 </template>
 
 <script>
 import { EnsoTable } from '@enso-ui/tables/bulma';
+import Avatar from '@enso-ui/users/src/bulma/pages/users/components/Avatar.vue';
+import Diff from './components/Diff.vue';
 
 export default {
     name: 'Index',
 
-    components: { EnsoTable },
+    components: { EnsoTable, Avatar, Diff },
 };
 </script>
 
